3e87c0521932bb5bb92fbcdd9000ba6d65ff2189
Matt Botz and Stephen Emery <botzmatt@yahoo.com>
2016-06-27 19:23:45 -0700

    adds user interface logic to scripts.js to take and store inputs from form

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,21 @@ function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
 }
+
+// UI Logic
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contact").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
