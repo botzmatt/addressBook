7a2db881ecc321bbdbea2ce1e796c09352562724
Matt Botz and Stephen Emery <emery.stephen@gmail.com>
2016-06-27 19:46:16 -0700

    add business logic to take input from first and last name fields to create a fullName variable. Also updates the contacts shown with fullName variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
+// business logic
+
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
 }
 
 // UI Logic
@@ -14,7 +20,7 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
