dbe38f8c81f70c9f12c92840d85283acfaaf7acc
Matt Botz and Stephen Emery <emery.stephen@gmail.com>
2016-06-27 20:14:03 -0700

    Add picture input and output in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 // business logic
 
-function Contact(first, last) {
+function Contact(first, last, email, picture) {
   this.firstName = first;
   this.lastName = last;
+  this.email = email;
+  this.picture = picture;
 }
 
 Contact.prototype.fullName = function() {
@@ -17,8 +19,10 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
+    var inputtedEmail = $("input#new-email").val();
+    var inputtedPicture = $("input#new-picture").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedEmail, inputtedPicture);
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
@@ -27,9 +31,8 @@ $(document).ready(function() {
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $(".email").html("<a href='mailto:" + newContact.email + "'>" + newContact.email + "</a>");
+      $(".picture").html("<img src='" + newContact.picture + "' alt='" + newContact.fullName + "'/>");
     });
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
   });
 });
