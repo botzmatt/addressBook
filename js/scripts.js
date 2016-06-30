d2f0f47d02a7f54350a5f08a890e6d3241f4cf92
Matt Botz and Stephen Emery <botzmatt@yahoo.com>
2016-06-29 18:53:04 -0700

    Add new address click function in scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -46,4 +46,21 @@ $(document).ready(function() {
     $("input#new-email").val("");
     $("input#new-picture").val("");
   });
+
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                '<div class="form-group">' +
+                                  '<label for="new-street">Street</label>' +
+                                  '<input type="text" class="form-control new-street">' +
+                                '</div>' +
+                                '<div class="form-group">' +
+                                  '<label for="new-city">City</label>' +
+                                  '<input type="text" class="form-control new-city">' +
+                                '</div>' +
+                                '<div class="form-group">' +
+                                  '<label for="new-state">State</label>' +
+                                  '<input type="text" class="form-control new-state">' +
+                                '</div>' +
+                              '</div>');
+  });
 });
